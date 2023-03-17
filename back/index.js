19ef8c1d509566ebf98ebcc406b98146af7e2ce6
MathisZerbib <mathis.zerbib@gmail.com>
2023-03-17 01:38:31 +0100

    initial commit 1

---
+++ back/index.js
@@ -0,0 +1,119 @@
+// Import dependencies
+const express = require("express");
+const Tesseract = require("tesseract.js");
+const multer = require("multer");
+const nlp = require("fr-compromise");
+nlp.extend(require("compromise-numbers"));
+
+// Initialize Express
+const app = express();
+const port = 3000; // Or any other port
+
+// Setting up multer middleware to receive images.
+const upload = multer({ dest: "uploads/" });
+
+// Initialize results array
+const results = [];
+
+// Define a route for image recognition
+app.post("/recognize", upload.array("files[]"), async (req, res, next) => {
+  try {
+    let files = req.files;
+    if (!files || !files.length)
+      return res.status(400).json({ message: "No files were uploaded." });
+
+    // Loop through all uploaded files
+    for (let i = 0; i < files.length; i++) {
+      const {
+        data: { text },
+      } = await Tesseract.recognize(files[i].path, "fra");
+
+      const { name, phone, email, raw } = processText(text);
+
+      results.push({ name, phone, email, raw });
+    }
+
+    res.json(results);
+  } catch (error) {
+    // Handle the error appropriately
+    console.error(error); // Log the error
+    next(error);
+  }
+});
+
+// Process extracted text
+function processText(text) {
+  const doc = nlp(text);
+
+  let highlight = {
+    nouns: doc.match("#Noun"),
+    verbs: doc.match("#Verb"),
+    adj: doc.match("#Adjective"),
+    adv: doc.match("#Adverb"),
+    det: doc.match("#Determiner"),
+    conj: doc.match("#Conjunction"),
+    prep: doc.match("#Preposition"),
+    num: doc.match("#Value"),
+    penn: doc.compute("penn"),
+    surname: doc.match("#Person"),
+  };
+
+  highlight = Object.keys(highlight).reduce((acc, key) => {
+    acc[key] = highlight[key].out("array");
+    return acc;
+  }, {});
+
+  const phones = extractPhone(text);
+  const emails = extractEmail(text);
+  const names = extractName(text);
+
+  // Clean phone number
+  if (phones) {
+    phones[0] = phones[0].replace(/[\s\(\)\-\,\.]/g, "");
+  }
+
+  let raw = highlight.penn.map((element) => {
+    // Remove unwanted characters
+    element = element.replace(/[^\w\s\p{L}]/gu, "");
+
+    // Remove leading/trailing white space
+    element = element.trim();
+
+    return element;
+  });
+
+  const name = names ? names[0] : "";
+  const phone = phones ? phones[0] : "";
+  const email = emails ? emails[0] : "";
+
+  return { name, phone, email, raw };
+}
+
+// Extract phone number from text
+function extractPhone(text) {
+  const phoneRegex = /\+?([\d|\(][\h|\(\d{3}\)|\.|\,|\-|\d]{4,}\d)/;
+  return text.match(phoneRegex);
+}
+
+//Extract email from text
+function extractEmail(text) {
+  const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+  return text.match(emailRegex);
+}
+
+// Extract name from text
+function extractName(text) {
+  const nameRegex = /^([A-Z][a-z]+\s){2}/;
+  return text.match(nameRegex);
+}
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("An error occurred while processing the image.");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
---
+++ front/src/App.js
@@ -0,0 +1,242 @@
+import React, { useState } from "react";
+import Dropzone from "react-dropzone";
+import axios from "axios";
+import {
+  Button,
+  Card,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  Typography,
+} from "@mui/material";
+
+function Name({ name }) {
+  return (
+    <div style={{ marginBottom: "1rem" }}>
+      <Typography variant="h6">Name:</Typography>
+      <Typography variant="body1">{name}</Typography>
+    </div>
+  );
+}
+
+function Phone({ phone }) {
+  return (
+    <div style={{ marginBottom: "1rem" }}>
+      <Typography variant="h6">Phone:</Typography>
+      <Typography variant="body1">{phone}</Typography>
+    </div>
+  );
+}
+
+function Email({ email }) {
+  return (
+    <div style={{ marginBottom: "1rem" }}>
+      <Typography variant="h6">Email:</Typography>
+      <Typography variant="body1">{email}</Typography>
+    </div>
+  );
+}
+
+function App() {
+  const [currentPreview, setCurrentPreview] = useState(null);
+  const [imagePreviews, setImagePreviews] = useState([]);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [raw, setRaw] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const onDropHandler = (acceptedFiles) => {
+    setImagePreviews(
+      acceptedFiles.map((file, index) => ({
+        preview: URL.createObjectURL(file),
+        index,
+      }))
+    );
+  };
+
+  const openModal = (index) => {
+    setCurrentPreview(index);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setCurrentPreview(null);
+    setIsModalOpen(false);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    // create FormData object to send file as multipart/form-data
+    const formData = new FormData();
+
+    imagePreviews.forEach((preview) => {
+      // Send an HTTP GET request to the image URL to get the raw image file.
+      axios.get(preview.preview, { responseType: "blob" }).then((response) => {
+        // Convert the response data into a Blob object.
+        const imageFile = new File([response.data], "image.jpg", {
+          type: response.headers["content-type"],
+        });
+
+        formData.append("files[]", imageFile);
+
+        if (formData.getAll("files[]").length === imagePreviews.length) {
+          // send POST request to backend URL
+          axios
+            .post("http://localhost:3000/recognize", formData)
+            .then((response) => {
+              console.log(response.data);
+              if (response.data[0].name.length > name.length)
+                setName(response.data[0].name);
+              if (response.data[0].phone.length > phone.length)
+                setPhone(response.data[0].phone);
+              if (response.data[0].email.length > email.length)
+                setEmail(response.data[0].email);
+              if (response.data[0].raw.length > raw.length)
+                setRaw(response.data[0].raw);
+            });
+        }
+      });
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        background:
+          "linear-gradient(to bottom right, rgba(220,220,220,0.2), rgba(255,255,255,0.2))",
+        padding: "2rem",
+      }}
+    >
+      <Card
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "50vw",
+          padding: "1rem",
+        }}
+      >
+        <Typography variant="h4" style={{ marginBottom: "1rem" }}>
+          JobMe CV Converter
+        </Typography>
+        <form onSubmit={onSubmitHandler}>
+          <Dropzone onDrop={onDropHandler}>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  minHeight: "20vh",
+                  border: "2px dashed grey",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                  margin: "1rem",
+                }}
+                {...getRootProps()}
+              >
+                <input {...getInputProps()} />
+                <Typography variant="body1">
+                  Drag and drop business card images here or click to select
+                  files
+                </Typography>
+              </div>
+            )}
+          </Dropzone>
+          {imagePreviews.length > 0 && (
+            <Grid container spacing={2}>
+              {imagePreviews.map((preview) => (
+                <Grid item xs={12} md={4} key={preview.index}>
+                  <Card
+                    style={{ position: "relative", overflow: "hidden" }}
+                    onClick={() => openModal(preview.index)}
+                  >
+                    <img
+                      src={preview.preview}
+                      alt=""
+                      style={{ width: "100%" }}
+                    />
+                    <div
+                      style={{
+                        position: "absolute",
+                        bottom: 0,
+                        left: 0,
+                        width: "100%",
+                        background: "rgba(0, 0, 0, 0.6)",
+                        color: "white",
+                        padding: "0.5rem",
+                        textOverflow: "ellipsis",
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                      }}
+                    >
+                      {preview.preview.substring(
+                        preview.preview.lastIndexOf("/") + 1
+                      )}
+                    </div>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          )}
+          <Button
+            type="submit"
+            variant="contained"
+            style={{ marginTop: "1rem" }}
+          >
+            Recognize
+          </Button>
+        </form>
+      </Card>
+      {isModalOpen && currentPreview !== null && (
+        <Dialog open={isModalOpen} onClose={closeModal}>
+          <DialogTitle>
+            {imagePreviews[currentPreview].preview.substring(
+              imagePreviews[currentPreview].preview.lastIndexOf("/") + 1
+            )}
+          </DialogTitle>
+          <DialogContent>
+            <img
+              src={imagePreviews[currentPreview].preview}
+              alt=""
+              style={{ width: "100%" }}
+            />
+          </DialogContent>
+        </Dialog>
+      )}
+      {name || phone || email || raw ? (
+        <div style={{ marginTop: "1rem" }}>
+          <Typography variant="h5" gutterBottom>
+            Recognized Text
+          </Typography>
+          {name && Name({ name })}
+          {phone && Phone({ phone })}
+          {email && Email({ email })}
+          {raw && (
+            <Grid item xs={12}>
+              <Typography variant="h6" style={{ marginTop: "1rem" }}>
+                {raw.length > 0 && "Raw text:"}
+              </Typography>
+              {raw.map((sentence, index) => (
+                <Typography variant="body1" key={index}>
+                  {sentence}
+                </Typography>
+              ))}
+            </Grid>
+          )}
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default App;
